0925dcf8a895a21f5dba5f9995375f2e3271d6a7
elbence <juanjopc08@gmail.com>
2022-02-15 14:18:27 +0100

    test1

--- src/main/java/CSV.java
+++ src/main/java/CSV.java
@@ -1,2 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CSV {
+    public Table readTable (String route) {
+
+        try {
+            File myFile = new File(route);
+            Scanner myIter = new Scanner(myFile);
+            while(myIter.hasNext()) {
+                String data = myIter.next();
+                System.out.println(data);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: File does not exist.");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
