8e0a1b1f40800581ba06b0c70f210f09cb4e7ea6
juanjo <juanjopc08@gmail.com>
2022-02-28 13:35:56 +0100

    created row (missing getData)

---
+++ src/main/java/Row.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Row {
+
+    private List<Double> data;
+
+    public Row() {
+        data = new LinkedList<>();
+    }
+
+    // ret 0 if succes; -1 if fail
+    public int addItem (Double item) {
+        if (item != null) {
+            data.add(item);
+            return 0;
+        }
+        return -1;
+    }
+
+}
