10732600efcad98e5ad851fdc388b91ceb88e2e4
mehdi <mehdigassabarsa@gmail.com>
2022-03-04 12:44:10 +0100

    Table completed

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,2 +1,24 @@
+import java.util.List;
+
 public class Table {
+    private List<String> headers;
+    private List<Row> rows;
+    public Table(){
+        rows = null;
+        headers = null;
+    }
+
+    public List<Double> getClumAt(int columNumber){
+        return rows.get(columNumber).getData();
+    }
+    public Row getRowAt(int rowNumber){
+        return rows.get(rowNumber);
+    }
+    public boolean addRow(Row row){
+       return rows.add(row);
+    }
+    public boolean addHeader(String header){
+        return headers.add(header);
+    }
+
 }
