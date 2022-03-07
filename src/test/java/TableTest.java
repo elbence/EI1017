7a1bf76ec6eb08bc2cecda05690cbbdf6d0fefbb
mehdi <mehdigassabarsa@gmail.com>
2022-03-07 13:44:14 +0100

    TableTest

---
+++ src/test/java/TableTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableTest {
+
+    @org.junit.jupiter.api.Test
+    void getClumAt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRowAt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addRow() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addHeader() {
+    }
+}
