ddbea530b31d0accabb41a3250d060b06cf73c45
juanjo <juanjopc08@gmail.com>
2022-03-24 00:03:13 +0100

    added some tests...

--- src/test/java/Algoritmos/KMeansTest.java
+++ src/test/java/Algoritmos/KMeansTest.java
@@ -33,8 +33,19 @@ class KMeansTest {
     @Test
     void estimate() {
 
-        KMeans kmeans = new KMeans(3,5, 234521);
+        KMeans kmeans;
+        String guess;
+
+        kmeans = new KMeans(3,5, 234521);
         kmeans.train(table);
+        guess = kmeans.estimate(table.getRowAt(15));
+        assertEquals("cluster-1", guess);
+        System.out.println();
+
+        kmeans = new KMeans(3,10, 234521);
+        kmeans.train(table);
+        guess = kmeans.estimate(table.getRowAt(132));
+        assertEquals("cluster-2", guess);
         System.out.println();
 
     }
