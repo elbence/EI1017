70872693dbf0780a72506bd69fae0bc5aa452744
mehdi <mehdigassabarsa@gmail.com>
2022-04-05 14:12:43 +0200

    finalizado arreglar herencia

--- src/main/java/Estructura/TableWithLabels.java
+++ src/main/java/Estructura/TableWithLabels.java
@@ -6,21 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TableWithLabels extends Table{
-    private List<RowWithLabels> rows;
     public TableWithLabels(){
         super();
-        rows = new ArrayList<>();
     }
-
-    public int size() {return rows.size();}
 
     @Override
     public RowWithLabels getRowAt(int rowNumber) {
-        return (RowWithLabels) rows.get(rowNumber);
-    }
-
-    public boolean addRow(RowWithLabels row){
-        return rows.add(row);
+        return (RowWithLabels) super.getRowAt(rowNumber);
     }
 
 
@@ -28,7 +20,8 @@ public class TableWithLabels extends Table{
     public String toString() {
         String tmp = "";
         tmp += super.getAllHeaders();
-        for (RowWithLabels row : rows) {
+        for (int i =0; i < super.size(); i++) {
+            RowWithLabels row = (RowWithLabels)  super.getRowAt(i);
             tmp += ("\n" + row);
         }
         return tmp;
