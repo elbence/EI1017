1475b5dda87b45ded08a4baaf0765b0734364f35
El Mehdi Gassa <49795916+Mehdi1017@users.noreply.github.com>
2022-05-26 16:39:36 +0200

    minor change

--- src/main/java/Estructura/Row.java
+++ src/main/java/Estructura/Row.java
@@ -7,12 +7,12 @@ import java.util.List;
 public class Row {
 
     private List<Double> data;
+    public Row() {
+        data = new ArrayList<>();
+    }
 
     public Row(List<Double> datos){
         data = datos;
-    }
-    public Row() {
-        data = new ArrayList<>();
     }
 
     public int size() {return data.size();}
@@ -39,27 +39,9 @@ public class Row {
 
     public Double get(int index) {return data.get(index);}
 
-    // compares parsed ROW to the row from which is called
-    // returns an abs value for expresing distance or -1 if rows are not comparable
-    /*public Double distanceTo(Row row) {
-        if (row.getData().size() == data.size()) {
-            Iterator<Double> iter1 = row.getData().iterator();
-            Iterator<Double> iter2 = data.iterator();
-            Double dist = 0.0;
-            while (iter1.hasNext()) dist += Math.pow(iter1.next() - iter2.next(), 2);
-            dist = Math.pow(dist, 0.5);
-            return dist;
-        }
-        return -1.0;
-    }*/
-
     @Override
     public String toString() {
         return data.toString();
     }
-
-    // recommended remove
-    // recommended equals
-    // more methods...
 
 }
